7084d624ce7dfd0ca8291ddfcb62e40e3881bcd3
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-21 09:49:43 +0200

    added solution

---
+++ FactoriesTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FactoriesTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
