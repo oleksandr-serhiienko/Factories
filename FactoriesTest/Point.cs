f4d4cd5ab1a93f64a1e819006d347e1bfcc07b2d
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-21 10:21:55 +0200

    To solve this problem we need to add Coordinate system var and add switch case

--- FactoriesTest/Point.cs
+++ FactoriesTest/Point.cs
@@ -6,13 +6,24 @@ namespace FactoriesTest
 {
     public class Point
     {
+        public enum CoordinateSystem
+        {
+            Cartesian,
+            Polar
+        }
+
         public double X { get; set; }
         public double Y { get; set; }
 
-        public Point(double x, double y)
+        public Point(double a, double b, 
+            CoordinateSystem system = CoordinateSystem.Cartesian)
         {
-            X = x;
-            Y = y;
+            (X, Y) = system switch 
+            {
+                CoordinateSystem.Cartesian => (a, b),
+                CoordinateSystem.Polar => (a*Math.Cos(b), a*Math.Sin(b)),
+                _=> throw new ArgumentNullException()
+            };
         }
 
         // is not possible to add the same signature for another values
