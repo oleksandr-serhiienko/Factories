4d49ef182846a4afaa8280a0161aa13b7337141f
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-21 11:05:30 +0200

    the way to use if point is going to be used in the same assemble

--- FactoriesTest/Point.cs
+++ FactoriesTest/Point.cs
@@ -18,7 +18,7 @@ namespace FactoriesTest
         public double X { get; set; }
         public double Y { get; set; }
 
-        internal Point(double x, double y)
+        private Point(double x, double y)
         {
             X = x;
             Y = y;
@@ -27,6 +27,15 @@ namespace FactoriesTest
         public override string ToString()
         {
             return $"X : {X} Y: {Y}";
+        }
+
+        public static class Factory
+        {
+            public static Point NewCarhesianPoint(double x, double y) => new Point(x, y);
+
+            public static Point NewPolarPoint(double rho, double theta) => new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+
+
         }
     }
 }
--- FactoriesTest/Program.cs
+++ FactoriesTest/Program.cs
@@ -13,7 +13,7 @@ namespace FactoriesTest
             //var test = new AsyncFactoryMethod();
             //await test.InitAsync();
 
-            var point = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            var point = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
             Console.WriteLine(point);
 
             //var x = await AsyncFactoryMethod.CreateAsync();
