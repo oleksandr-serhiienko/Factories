6350a30e8748dce4790e68c44e98dccb98ce1e70
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-21 09:58:59 +0200

    the first problem is that we can not use constractor with the same signature to pass the other values

---
+++ FactoriesTest/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoriesTest
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // is not possible to add the same signature for another values
+        // one of the reason to use facotires
+        //public Point(double rho, double theta)
+        //{
+
+        //}
+
+    }
+}
