f58fbcfcd188b580ff9fb9bd5b731e2440fff4b1
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-21 17:30:19 +0200

    added a task

---
+++ FactoriesTest/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoriesTest
+{
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string  Name { get; set; }
+
+        public Person( int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
---
+++ FactoriesTest/PersonFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FactoriesTest
+{
+    public class PersonFactory
+    {
+        public Person CreatePerson(string name)
+        {
+            var person = new Person(Persons.Count, name);
+            Persons.Add(person);
+            return person;
+        }
+
+        private List<Person> Persons { get; set; } = new List<Person>();
+    }
+}
