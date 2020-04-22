89612c990896d4557ca285826733d13666779035
Oleksandr Serhiienko <alex.ser.online@gmail.com>
2020-04-22 11:02:42 +0200

    made task

--- TestPrototypes/Program.cs
+++ TestPrototypes/Program.cs
@@ -12,6 +12,12 @@ namespace TestPrototypes
             jane.Adress.HouseNumber = 222;
             Console.WriteLine(john);
             Console.WriteLine(jane);
+
+            var line = new Line(new Point(2,2), new Point(3,1));
+            var line1 = line.DeepCopy();
+            line1.End.Y = 2;
+            Console.WriteLine(line);
+            Console.WriteLine(line1);
         }
     }
 }
---
+++ TestPrototypes/Task.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestPrototypes
+{
+
+    public interface IPrototype<T>
+    {
+        T DeepCopy();
+    }
+
+    public class Point : IPrototype<Point>
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public Point()
+        { 
+        }
+
+        public Point DeepCopy()
+        {
+            return new Point(X, Y);
+        }
+    }
+
+    public class Line : IPrototype<Line>
+    {
+        public Point Start, End;
+
+        public Line()
+        { 
+        }
+
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Line DeepCopy()
+        {
+            return new Line(Start.DeepCopy(), End.DeepCopy());
+        }
+
+        public override string ToString()
+        {
+            return $"Start Point: {Start.X}, {Start.Y}. End Point: {End.X}, {End.Y}";
+        }
+    }
+}
